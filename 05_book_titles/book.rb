87637b4ec835aceae947700eb8bf3516a7979690
Peter <peto.mester@gmail.com>
2017-01-07 20:47:20 +0100

    finished 05-06

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def title=(new_tit)
+    arr=new_tit.split
+    @title = arr[0].capitalize
+    arr[1..-1].each do |word|
+      if word.match(/\b(a|an|the|and|with|in|of)\b/)
+        @title = @title + ' ' + word
+      else
+        @title = @title + ' ' + word.capitalize
+      end
+    end
+    @title
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,32 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+  def time_string
+    h=0
+    m=0
+    s=0
+    left = @seconds
+    if left/3600 >= 1
+      h = left/3600
+      left = left - h*3600
+    end
+    if left/60 >= 1
+      m = left/60
+      left = left - m*60
+    end
+    s = left
+
+    def form x
+      x = x.to_s
+      if !(x.match(/\w+{2}/))
+        x = '0'+x
+      end
+      x
+    end
+
+    (form h) +':'+(form m) +':'+(form s)
+  end
 end
